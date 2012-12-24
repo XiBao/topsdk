5ae7fc001b9f620cb521376a06a96a7bf198a70f
Syd Xu <prof.syd.xu@gmail.com>
2012-12-24 15:23:57 +0800

    update topsdk

--- request/TmallTemaiItemsSearchRequest.php
+++ request/TmallTemaiItemsSearchRequest.php
@@ -3,7 +3,7 @@
  * TOP API: tmall.temai.items.search request
  * 
  * @author auto create
- * @since 1.0, 2012-12-20 16:37:10
+ * @since 1.0, 2012-12-24 12:33:33
  */
 class TmallTemaiItemsSearchRequest
 {
--- request/TopatsResultGetRequest.php
+++ request/TopatsResultGetRequest.php
@@ -3,7 +3,7 @@
  * TOP API: taobao.topats.result.get request
  * 
  * @author auto create
- * @since 1.0, 2012-12-20 16:37:10
+ * @since 1.0, 2012-12-24 12:33:33
  */
 class TopatsResultGetRequest
 {
