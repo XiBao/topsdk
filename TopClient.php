737651c7093744b6deeae6aa6e2a02f024838725
Syd Xu <thepeppersstudio@gmail.com>
2016-04-15 18:26:35 +0800

    fix a bug in JSON decode

--- TopClient.php
+++ TopClient.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'JSON.php';
+
 class TopError {
     public $code;
     public $msg;
@@ -203,7 +205,7 @@ class TopClient
 			//$json = str_replace(array("\n","\r", "\t"),"",$resp); 
     		//$json = preg_replace('/([{,]+)(\s*)([^"]+?)\s*:/','$1"$3":',$json);
     		$jsonService = new Services_JSON(SERVICES_JSON_LOOSE_TYPE);
-			$respObject = $jsonService.decode($json);
+			$respObject = $jsonService->decode($resp);
 			if (null !== $respObject)
 			{
 				$respWellFormed = true;
